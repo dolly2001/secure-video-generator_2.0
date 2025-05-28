ac4bf1e335af1be067e951d755eef6823a0b865d
Piyush Solanki <piyushsolanki18dec@gmail.com>
2025-05-29 00:59:12 +0530

    Image & Text Upload

--- app.py
+++ app.py
@@ -1,8 +1,68 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 
 @app.route('/')
 def route_1():
-    return 'project initiated'
+    return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    text = request.form.get('overlay_text')
+    image = request.files['image']
+
+    if image:
+        image_path = os.path.join(UPLOAD_FOLDER, image.filename)
+        image.save(image_path)
+        return f"Image Uploaded!! Text: {text}"
+    return "No Image uploaded."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
